d979e47617d06fdde45681a1000b1fe77cce6c5a
samiulalimsaad <samiulalimsaad@gmail.com>
2023-01-13 17:34:13 +0600

    eventToShow

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,18 +1,22 @@
+import { useState } from "react";
 import EventDetails from "./EventDetails";
+import { eventInterface } from "./interface/eventInterface";
 import LeftSidebar from "./LeftSidebar";
 import RightSideBar from "./RightSideBar";
 
 const Layout = () => {
+    const [eventToShow, setEventToShow] = useState<eventInterface>();
+
     return (
-        <div className="h-[calc(100vh-4rem)] grid grid-cols-12">
+        <div className="h-[calc(100vh-4rem)] grid grid-cols-12  overflow-hidden">
             <div className="col-span-9 grid grid-cols-12">
                 <LeftSidebar />
                 <div className="col-span-11">
-                    <EventDetails />
+                    {eventToShow && <EventDetails event={eventToShow} />}
                 </div>
             </div>
             <div className="col-span-3">
-                <RightSideBar />
+                <RightSideBar setEventToShow={setEventToShow} />
             </div>
         </div>
     );
