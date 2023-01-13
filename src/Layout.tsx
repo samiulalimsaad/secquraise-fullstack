15d298b69d4ea00d58b9a9eab278a806f3b06de7
samiulalimsaad <samiulalimsaad@gmail.com>
2023-01-13 16:25:47 +0600

    layout updated

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,9 +1,12 @@
 import LeftSidebar from "./LeftSidebar";
+import RightSideBar from "./RightSideBar";
 
 const Layout = () => {
     return (
-        <div className="h-[calc(100vh-4rem)]">
+        <div className="h-[calc(100vh-4rem)] grid grid-cols-3">
             <LeftSidebar />
+            <div>Content</div>
+            <RightSideBar />
         </div>
     );
 };
