4075189746346797e42b354b3d7e00d09fc1c873
samiulalimsaad <samiulalimsaad@gmail.com>
2023-01-13 17:01:36 +0600

    env issue fixed

--- src/firebase.init.ts
+++ src/firebase.init.ts
@@ -1,17 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: import.meta.env.process.env.REACT_APP_apiKey,
-    authDomain: import.meta.env.process.env.REACT_APP_authDomain,
-    projectId: import.meta.env.process.env.REACT_APP_projectId,
-    storageBucket: import.meta.env.process.env.REACT_APP_storageBucket,
-    messagingSenderId: import.meta.env.process.env.REACT_APP_messagingSenderId,
-    appId: import.meta.env.process.env.REACT_APP_appId,
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const database = getDatabase(app);
