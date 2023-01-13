27fb7ea58abfa7b9769e276a1dd6452ead921efd
samiulalimsaad <samiulalimsaad@gmail.com>
2023-01-13 16:11:48 +0600

    layout added

---
+++ src/Layout.tsx
@@ -0,0 +1,11 @@
+import LeftSidebar from "./LeftSidebar";
+
+const Layout = () => {
+    return (
+        <div className="h-[calc(100vh-4rem)]">
+            <LeftSidebar />
+        </div>
+    );
+};
+
+export default Layout;
