be3d7a23956ee2c97818b9e2898e3104ad787f6b
samiulalimsaad <samiulalimsaad@gmail.com>
2023-01-13 17:33:23 +0600

    show event on middle col

--- src/Event.tsx
+++ src/Event.tsx
@@ -1,8 +1,18 @@
+import { Dispatch, SetStateAction } from "react";
 import { eventInterface } from "./interface/eventInterface";
 
-const Event = ({ event }: { event: eventInterface }) => {
+const Event = ({
+    event,
+    setEventToShow,
+}: {
+    event: eventInterface;
+    setEventToShow: Dispatch<SetStateAction<eventInterface | undefined>>;
+}) => {
     return (
-        <div className="card w-full bg-slate-200 rounded-none my-3 hover:bg-slate-500 hover:text-white duration-300 cursor-pointer">
+        <div
+            className="card w-full bg-slate-200 rounded-none my-3 hover:bg-slate-500 hover:text-white duration-300 cursor-pointer"
+            onClick={() => setEventToShow(event)}
+        >
             <div className="card-body p-2">
                 <div className="flex items-center justify-between">
                     <h2 className="card-title">
