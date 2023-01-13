36eadf3f601ae84bd51d93984b8c4b503d258343
samiulalimsaad <samiulalimsaad@gmail.com>
2023-01-13 16:25:29 +0600

    events component added

---
+++ src/Events.tsx
@@ -0,0 +1,15 @@
+import Event from "./Event";
+
+const Events = () => {
+    return (
+        <div>
+            <Event />
+            <Event />
+            <Event />
+            <Event />
+            <Event />
+        </div>
+    );
+};
+
+export default Events;
