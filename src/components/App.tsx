fe7a646b6ee0369bdc9cdcb933e7c71c1db8c95a
samiulalimsaad <samiulalimsaad@gmail.com>
2023-01-14 01:34:04 +0600

    title updated

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,12 @@
+import { useEffect } from "react";
 import Layout from "../utils/Layout";
 import Navbar from "./Navbar";
 
 function App() {
+    useEffect(() => {
+        document.title = "secquraise-fullstack-job";
+    }, []);
+
     return (
         <div className="h-screen">
             <Navbar />
