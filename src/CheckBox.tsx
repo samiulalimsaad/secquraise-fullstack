469f38597f0ff150446fd194fda0f2a8eabd8667
samiulalimsaad <samiulalimsaad@gmail.com>
2023-01-14 00:46:09 +0600

    CheckBox component added

---
+++ src/CheckBox.tsx
@@ -0,0 +1,37 @@
+import { MouseEventHandler, useEffect, useState } from "react";
+import { filterInterface } from "./RightSideBar";
+
+const CheckBox = ({
+    filter,
+    value,
+    onClick,
+}: {
+    filter: filterInterface | null;
+    value: string;
+    onClick: MouseEventHandler<HTMLLabelElement>;
+}) => {
+    const [checked, setChecked] = useState(false);
+
+    useEffect(() => {
+        setChecked(value === filter?.value);
+    }, [filter]);
+
+    return (
+        <div className="form-control">
+            <label
+                className="cursor-pointer label justify-start gap-4"
+                onClick={onClick}
+            >
+                <input
+                    type="checkbox"
+                    checked={checked}
+                    className="checkbox checkbox-success"
+                    onChange={(e) => setChecked(e.target.checked)}
+                />
+                <span className="label-text capitalize">{value}</span>
+            </label>
+        </div>
+    );
+};
+
+export default CheckBox;
