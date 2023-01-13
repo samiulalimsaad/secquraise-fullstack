0b57f6ff91b1eeef4937c25996c2ca29406d6794
samiulalimsaad <samiulalimsaad@gmail.com>
2023-01-13 16:11:52 +0600

    updated

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Layout from "./Layout";
+import Navbar from "./Navbar";
+
+function App() {
+    return (
+        <div className="h-screen">
+            <Navbar />
+            <Layout />
+        </div>
+    );
+}
+
+export default App;
