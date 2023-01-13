a7d75a80f5d08b6674631d73b0e7baee0ff7feab
samiulalimsaad <samiulalimsaad@gmail.com>
2023-01-13 17:05:42 +0600

    showing all evetns

--- src/Event.tsx
+++ src/Event.tsx
@@ -1,12 +1,16 @@
-const Event = () => {
+import { eventInterface } from "./interface/eventInterface";
+
+const Event = ({ event }: { event: eventInterface }) => {
     return (
         <div className="card w-full bg-slate-200 rounded-none my-3 hover:bg-slate-500 hover:text-white duration-300 cursor-pointer">
             <div className="card-body p-2">
                 <div className="flex items-center justify-between">
-                    <h2 className="card-title">EVT0050: Chennai</h2>
+                    <h2 className="card-title">
+                        {event.ID}: {event.Location}
+                    </h2>
                     <div className="flex items-center justify-between gap-2">
-                        <span>1/2/3</span>
-                        <span>1/2/3</span>
+                        <span>{event.Date}</span>
+                        <span>{event.Time}</span>
                     </div>
                 </div>
                 <p>Person Detected.</p>
--- src/Events.tsx
+++ src/Events.tsx
@@ -1,13 +1,20 @@
+import { ref } from "firebase/database";
+import { useList } from "react-firebase-hooks/database";
 import Event from "./Event";
+import { database } from "./firebase.init";
 
 const Events = () => {
+    const [snapshots, loading, error] = useList(ref(database, "/"));
+
+    console.log(snapshots);
+
     return (
         <div>
-            <Event />
-            <Event />
-            <Event />
-            <Event />
-            <Event />
+            {error && <strong>Error: {error.message}</strong>}
+            {loading && <span>List: Loading...</span>}
+            {!loading &&
+                snapshots &&
+                snapshots.map((v) => <Event key={v.key} event={v.val()} />)}
         </div>
     );
 };
