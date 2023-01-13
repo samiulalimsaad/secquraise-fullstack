737a113675dca2bf0f6c6d8503faecba3ad537d0
samiulalimsaad <samiulalimsaad@gmail.com>
2023-01-13 16:25:41 +0600

    RightSideBar added

---
+++ src/RightSideBar.tsx
@@ -0,0 +1,21 @@
+import { AdjustmentsHorizontalIcon } from "@heroicons/react/24/outline";
+import Events from "./Events";
+
+const RightSideBar = () => {
+    return (
+        <div className="h-full bg-slate-200">
+            <div className="m-2 bg-white card p-4 rounded-none">
+                <div className="flex items-center justify-between">
+                    <h2 className="text-xl font-semibold">Events</h2>
+                    <AdjustmentsHorizontalIcon className="h-8 w-8" />
+                </div>
+                <div className="divider"></div>
+                <div>
+                    <Events />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default RightSideBar;
