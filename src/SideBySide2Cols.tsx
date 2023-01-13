0b1294301788e86f0b7f3a9538b636f0b9b62017
samiulalimsaad <samiulalimsaad@gmail.com>
2023-01-13 17:34:21 +0600

    SideBySide2Cols updated

--- src/SideBySide2Cols.tsx
+++ src/SideBySide2Cols.tsx
@@ -1,10 +1,10 @@
-const SideBySide2Cols = () => {
+const SideBySide2Cols = ({ name, value }: { name: string; value: string }) => {
     return (
-        <div className="grid grid-cols-9">
-            <div className="col-span-3">
-                <span>Name</span>:
+        <div className="grid grid-cols-12">
+            <div className="col-span-2">
+                <span className="capitalize">{name}</span>:
             </div>
-            <div className="col-span-6 bg-error">Value</div>
+            <div className="col-span-10">{value}</div>
         </div>
     );
 };
