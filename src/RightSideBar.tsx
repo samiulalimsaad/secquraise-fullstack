a19a45747e07f2e8ac157cc00c2ca61978215ff6
samiulalimsaad <samiulalimsaad@gmail.com>
2023-01-13 23:53:44 +0600

    temporary filter added

--- src/RightSideBar.tsx
+++ src/RightSideBar.tsx
@@ -1,7 +1,7 @@
 import { AdjustmentsHorizontalIcon } from "@heroicons/react/24/outline";
 import { ref } from "firebase/database";
 import { Dispatch, SetStateAction, useEffect } from "react";
-import { useList } from "react-firebase-hooks/database";
+import { useListVals } from "react-firebase-hooks/database";
 import Event from "./Event";
 import { database } from "./firebase.init";
 import { eventInterface } from "./interface/eventInterface";
@@ -11,20 +11,28 @@ const RightSideBar = ({
 }: {
     setEventToShow: Dispatch<SetStateAction<eventInterface | undefined>>;
 }) => {
-    const [snapshots, loading, error] = useList(ref(database, "/"));
+    const [snapshots, loading, error] = useListVals(ref(database, "/"), {
+        transform: (val: eventInterface) =>
+            val.Location === "Chennai" ? val : null,
+    });
 
     useEffect(() => {
         if (!loading && snapshots && snapshots.length) {
-            setEventToShow(snapshots[0].val());
+            for (let i = 0; i < snapshots.length; i++) {
+                if (Object.keys(snapshots[i]).length > 0) {
+                    setEventToShow(snapshots[i]);
+                    break;
+                }
+            }
         }
-    }, [loading, snapshots, snapshots]);
+    }, [loading]);
 
     return (
         <div className="h-full bg-slate-200 w-full">
             <div className="m-2 bg-white card p-4 rounded-none">
                 <div className="flex items-center justify-between">
                     <h2 className="text-xl font-semibold">Events</h2>
-                    <a href="#filter-modal" className="btn">
+                    <a href="#filter-modal" className="">
                         <AdjustmentsHorizontalIcon className="h-8 w-8" />
                     </a>
                 </div>
@@ -39,13 +47,16 @@ const RightSideBar = ({
                         {loading && <span>Loading...</span>}
                         {!loading &&
                             snapshots &&
-                            snapshots.map((v) => (
-                                <Event
-                                    key={v.key}
-                                    event={v.val()}
-                                    setEventToShow={setEventToShow}
-                                />
-                            ))}
+                            snapshots.map(
+                                (v) =>
+                                    Object.keys(v).length > 0 && (
+                                        <Event
+                                            key={v.ID}
+                                            event={v}
+                                            setEventToShow={setEventToShow}
+                                        />
+                                    )
+                            )}
                     </div>
                 </div>
             </div>
